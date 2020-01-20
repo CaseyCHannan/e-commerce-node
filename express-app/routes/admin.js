2d4b6e0e9277a8a8d3919e46541a84ad42f015f8
Casey Hannan <Hannan.Casey@gmail.com>
2020-01-19 23:53:24 -0500

    statically serving html

--- express-app/routes/admin.js
+++ express-app/routes/admin.js
@@ -1,15 +1,14 @@
+const path = require('path')
+
+const rootDir = require('../utils/pathdir')
 const express = require('express')
 
 const router = express.Router();
 
-router
-
 router.get('/add-product', (req, res, next ) => {
-    console.log("expressivo")
-    // res.send('<h1> So, you want to add a product?</h1>')
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
-})
-router.post('/product',(req, res, next) => {
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
+    })
+router.post('/add-product',(req, res, next) => {
     console.log(req.body)
     res.redirect('/')
 })
